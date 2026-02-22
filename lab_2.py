e67a80d674c6f4a913784a87f6ace6a0060a40ee
Walter Mokwena <mokwenawalter50@gmail.com>
2026-02-22 11:49:16 +0000

    Copleted week 2 quizes

--- lab_2.py
+++ lab_2.py
@@ -1,1 +1,26 @@
-print('learning how to pull and push')
+#Submision 12: Rock-Paper-Scissors
+
+player1 = input()
+player2 = input()
+
+rock = "rock"
+paper = "paper"
+scissors = "scissors"
+
+# conditions for a draw
+if player1 == player2:
+    print("Tie")
+#conditions for player1 to win:
+    #player1 = rock and player2 = scisors
+    #player1 = paper and player2 = rock
+    #player1 = scisors and player2 = paper
+#else player2 
+ 
+elif player1 == rock and player2 == scissors:
+    print("Player 1 wins")
+elif player1 == paper and player2 == rock:
+    print("Player 1 wins")
+elif player1 == scissors and player2 == paper:
+    print("Player 1 wins")
+else:
+    print("Player 2 wins")
